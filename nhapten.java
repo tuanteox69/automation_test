0ebe62b4576f89d7f3149fcbe49f2756d769030f
tuanteox69 <103015804+tuanteox69@users.noreply.github.com>
2022-12-06 08:47:49 +0700

    Add files via upload

---
+++ nhapten.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class nhapten {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+            System.out.println("Nhập thông tin họ tên Học viên : ");
+String username = scanner.nextLine();
+        System.out.println(username);
+    }
+}
